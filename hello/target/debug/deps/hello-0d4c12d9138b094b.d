becd477acc199139f025d63d15ac32c623e34bf0
江トウ <>
2022-01-27 02:36:35 +0900

    hello world

---
+++ hello/target/debug/deps/hello-0d4c12d9138b094b.d
@@ -0,0 +1,5 @@
+/Users/lowkeyrojo/rust/hello/target/debug/deps/hello-0d4c12d9138b094b: src/main.rs
+
+/Users/lowkeyrojo/rust/hello/target/debug/deps/hello-0d4c12d9138b094b.d: src/main.rs
+
+src/main.rs:
